2f6bd8962d19ed923b314e89558f3157f26e6e49
syam-cs <syam.s@globallogic.com>
2019-11-22 18:22:43 +0530

    test

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver import FirefoxProfile
 import time
 
-profile = FirefoxProfile('/home/syam.s/.mozilla/firefox/uzdp3719.FirefoxProfileTest')
+profile = FirefoxProfile('/home/syam.s/.mozilla/firefox/t59w63h4.default')
 cap=webdriver.DesiredCapabilities.FIREFOX.copy()
 remote = webdriver.Remote("http://localhost:4444/wd/hub", desired_capabilities=cap,browser_profile=profile)
 
