1babe684ae02327d6207debc77b9a697799f0f1a
syam-cs <syam.s@globallogic.com>
2019-11-22 17:58:12 +0530

    test

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
 from selenium import webdriver
-from time import sleep
+from selenium.webdriver import FirefoxProfile
+import time
 
-fp=None
-fp = webdriver.FirefoxProfile()
-fp.set_preference("plugin.state.java", 2)
+profile = FirefoxProfile('/home/syam.s/.mozilla/firefox/uzdp3719.FirefoxProfileTest')
 cap=webdriver.DesiredCapabilities.FIREFOX.copy()
-remote = webdriver.Remote("http://localhost:4444/wd/hub", desired_capabilities=cap)
+remote = webdriver.Remote("http://localhost:4444/wd/hub", desired_capabilities=cap,browser_profile=profile)
 
 remote.get('https://www.eurosportplayer.com/')
-sleep(10)
+time.sleep(20)
 
-remote.quit() 
+remote.quit()
