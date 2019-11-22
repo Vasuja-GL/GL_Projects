f41139444351f74a7b0b7ceb303e45ff61fea41d
syam-cs <syam.s@globallogic.com>
2019-11-22 18:40:37 +0530

    test

--- test.py
+++ test.py
@@ -2,12 +2,14 @@ from selenium import webdriver
 from selenium.webdriver import FirefoxProfile
 import time
 
+profile = FirefoxProfile('/home/syam.s/.mozilla/firefox/t59w63h4.default')
 desiredCapabilities={
 "browserName":"firefox"
 }
 
-driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub',desired_capabilities = desiredCapabilities);
-driver.get("https://www.google.co.in/");
-print driver.title;
-driver.quit();
+remote = webdriver.Remote("http://localhost:4444/wd/hub", desired_capabilities=desiredCapabilities,browser_profile=profile)
 
+remote.get('https://www.eurosportplayer.com/')
+time.sleep(20)
+
+remote.quit()
