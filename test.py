88a7534af19854bc9b0c1b7ed9507a4a8bfa56ba
syam-cs <syam.s@globallogic.com>
2019-11-22 11:47:20 +0530

    test

--- test.py
+++ test.py
@@ -1,19 +1,10 @@
-import selenium
-import time
 from selenium import webdriver
 
+desiredCapabilities={
+"browserName":"chrome"
+}
 
-#driver =webdriver.Chrome('/usr/local/bin/chromedriver')
-
-options = webdriver.ChromeOptions()
-
-            #options.add_argument(r"--no-sandbox")
-           # options.add_argument('--profile-directory=Test')
-options.add_argument("--user-data-dir='/home/syam.s/.cache/google-chrome/Default/Cache'")
-options.add_argument("--profile-directory='Default'")
-#options.add_argument("--user-data-dir=/home/syam.s/.config/google-chrome/'Profile 1'")
-#options.add_argument("--profile-directory='Profile 1'")
-driver = webdriver.Chrome('/usr/bin/chromedriver',chrome_options=options)
-driver.execute_script("window.open('https://eurosportplayer.com');")
-time.sleep(10)
-driver.quit()
+driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub',desired_capabilities = desiredCapabilities);
+driver.get("https://www.google.co.in/");
+print driver.title;
+driver.quit();
